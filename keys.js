3249b1b9b2dce04b083e182c981af03f5ce3e6f7
Suha19 <47680905+Suha19@users.noreply.github.com>
2019-05-02 12:45:55 -0500

    #2 Api-dotevt

--- keys.js
+++ keys.js
@@ -4,3 +4,11 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+exports.ticketmaster = {
+  id: process.env.ticketmaster_Consumer_Key,
+  secret: process.env.ticketmaster_Consumer_Secret
+};
+exports.OMDb = {
+  key: process.env.OMDb_Key,
+  API: process.env.OMDb_API
+};
--- liri.js
+++ liri.js
@@ -1,3 +1,68 @@
 require("dotenv").config();
 const keys = require("./keys.js");
 const spotify = new Spotify(keys.spotify);
+
+
+var options = {
+  provider: "spotify",
+//   apiKey: "https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/"
+};
+
+// Get all elements in process.argv, starting from index 2 to the end
+// Join them into a string to get the space delimited concerts 
+var concerts  = process.argv.slice(2).join(" ");
+
+// Then use the Google spotify to spotify the concerts 
+spotify.spotify(concerts , function(err, data) {
+  console.log(JSON.stringify(data[0], null, 2));
+
+  var concerts  = data[0];
+
+  // Depending on what information is available for an concerts , build formatted search
+  var search = "";
+
+  if (concerts .name) {
+    search += concerts .name;
+  }
+
+  if (concerts .location) {
+    search += ", " + concerts .location;
+  }
+
+  if (concerts .date) {
+    search += "mm/dd/yyyy " + concerts .data;
+  }
+
+  if (concerts .countryCode) {
+    search += " " + concerts .countryCode;
+  }
+
+  concerts.find(function(err, result) {
+    // If there is insufficient data, notify the user.
+    if (err) {
+      console.log("\r\n\r\n\r\n");
+
+      console.log("Sorry we don't have enough data on that concerts! Try somewhere else.");
+
+      console.log("\r\n\r\n\r\n");
+
+      return;
+    }
+
+    // Then print the concerts information and complete concerts 
+    console.log("\r\n\r\n\r\n");
+
+    console.log("concerts " + search);
+
+    
+  });
+});
+
+axios({
+    method:'get',
+    url:'http://www.omdbapi.com/?i=tt3896198&apikey=OMDb_Key',
+    responseType:'stream'
+  })
+    .then(function(response) {
+    response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+  });
